06981651300e91dc9529cec0c607b3a92d1a7a0b
AlessandroSchuquelPedroso <alessandropedrosoti@gmail.com>
2019-09-23 20:38:49 -0300

    exemplo de uma api básico em spring-java

---
+++ wsfsas2/comerical-api/src/main/java/com/algaworks/comercial/ComericalApiApplication.java
@@ -0,0 +1,13 @@
+package com.algaworks.comercial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComericalApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComericalApiApplication.class, args);
+	}
+
+}
